2845f87d533527a441e4876ca0e719ef5b3776c1
GeonwooShin <rjsdnslawkd@naver.com>
2024-04-16 15:44:52 +0900

    feat: input date의 포맷을 수정하기 위한 유틸 함수 작성

--- src/utils/getFormatDate.ts
+++ src/utils/getFormatDate.ts
@@ -4,3 +4,15 @@ export const getFormatDate = (date: Date) => {
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}${month}${day}`;
 };
+
+export const getDivideFormatDate = (date: string): string => {
+  const year = date.slice(0, 4);
+  const month = date.slice(4, 6);
+  const day = date.slice(6, 8);
+
+  return `${year}-${month}-${day}`;
+};
+
+export const removeHyphenFromDate = (dateString: string) => {
+  return dateString.replace(/-/g, '');
+};
